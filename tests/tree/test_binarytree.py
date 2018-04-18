27cf8a2fdb6dae01a58e903a856582a00957bfe7
Jonathan Madden <virtualmadden@users.noreply.github.com>
2018-04-17 17:02:53 -0700

    fleshing out tests

--- tests/tree/test_binarytree.py
+++ tests/tree/test_binarytree.py
@@ -8,3 +8,14 @@ class Test(unittest.TestCase):
 
     def test_shouldInsertNode(self):
         self.assertTrue(self.tree.insert(40))
+
+    def test_shouldNotInsertSameNode(self):
+        self.tree.insert(40)
+
+        self.assertFalse(self.tree.insert(40))
+
+    def test_shouldNotInsertSameNode(self):
+        self.tree.insert(40)
+        self.tree.insert(60)
+
+        self.assertTrue(self.tree.root.right is not None and self.tree.root.left is not None)
